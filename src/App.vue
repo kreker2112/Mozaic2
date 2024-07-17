929bc0cfff34ad5b7433d89020eda85f291f0570
kreker2112 <kreker2112@gmail.com>
2024-07-17 14:54:13 +0300

    some updates

--- src/App.vue
+++ src/App.vue
@@ -28,18 +28,10 @@ export default {
 
 .page-container {
   width: 100vw;
-  height: calc(100% - 4rem); /* Adjust height considering the footer */
-  /* overflow: hidden; */
+  height: calc(100% - 4rem);
   position: relative;
 }
 
-/* .page {
-  height: 100%;
-  width: 100vw;
-  position: absolute;
-  transition: all 1s;
-  overflow: hidden;
-} */
 @media only screen and (min-width: 3840px) and (max-width: 3840px) and (min-height: 2045px) and (max-height: 2045px) {
   .page-container {
     height: calc(100% - 9rem);
